01938bd0410dce1e02d89a5ff37ad5e455544276
Kirubel Kassaye <KirubelKa@moenco.corp>
2022-07-18 16:27:02 +0300

    Add basic classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,37 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(Elibrary());
+
+class Elibrary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+              child: Text('E-library')),
+        ),
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+            child: ebook(),
+        ),
+      ),
+    );
+  }
+}
+
+class ebook extends StatefulWidget {
+  @override
+  State<ebook> createState() => _ebookState();
+}
+
+class _ebookState extends State<ebook> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+          'Selam Ethiopia',
+    ),
+    );
+  }
+}
